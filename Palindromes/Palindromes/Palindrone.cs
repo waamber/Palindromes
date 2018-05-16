4fe5a2bbfd95b85628dd77610027e791922d6368
waamber <amberastuart@aol.com>
2018-05-15 20:11:35 -0500

    ahh

---
+++ Palindromes/Palindromes/Palindrone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Palindromes
+{
+	class Palindrone
+	{
+	
+		public void IsItAPalindrome(string sentence)
+		{
+
+			var removedNonChars = new string(sentence.Where(Char.IsLetter).ToArray());
+			string[] split = removedNonChars.ToLower().Split();
+			var joined = "";
+
+			foreach(var word in split)
+			{
+				joined += word;
+			}
+
+			char[] joinedArray = joined.ToCharArray();
+			Array.Reverse(joinedArray);
+
+			var reversed = new string(joinedArray);
+
+			if(reversed == joined)
+			{
+				Console.WriteLine("True");
+			}
+			else Console.WriteLine("False");
+
+		}
+	}
+}
---
+++ Palindromes/Palindromes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var reverse = new Palindrone();
+			reverse.IsItAPalindrome("taco2!cat");
+			Console.ReadLine();
+		}
+	}
+}
